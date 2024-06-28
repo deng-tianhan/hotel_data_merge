d593cfa8127441366dcd3f5bca7aaa8078e2a541
Tianhan <tianhand@gmail.com>
2024-06-28 13:57:41 +0800

    add method and test for hotel model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,9 @@
 class Hotel < ApplicationRecord
+  validates_presence_of :identifier
+
+  def self.create_from(attributes)
+    hotel = self.where(identifier: attributes[:identifier]).first_or_initialize
+    hotel.update!(attributes)
+    return hotel
+  end
 end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Hotel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:identifier) { 'Abc123' }
+
+  describe 'validation' do
+    subject { Hotel.new(identifier: identifier) }
+
+    it 'should be valid' do
+      expect(subject).to be_valid
+    end
+
+    context 'identifier is missing' do
+      let(:identifier) { nil }
+      it { expect(subject).to be_invalid }
+    end
+
+    context 'identifier is blank' do
+      let(:identifier) { nil }
+      it { expect(subject).to be_invalid }
+    end
+  end
+
+  describe '.create_from' do
+    let(:attributes) { { identifier: identifier } }
+
+    it 'creates new hotel' do
+      expect{ Hotel.create_from(attributes) }.to change{ Hotel.count }.by(1)
+    end
+
+    it 'returns the hotel' do
+      expect(Hotel.create_from(attributes)).to be_an_instance_of(Hotel)
+    end
+
+    context 'hotel already exists' do
+      let!(:existing_hotel) { Hotel.create(identifier: identifier) }
+
+      it 'does not create new hotel' do
+        expect{ Hotel.create_from(attributes) }.not_to change{ Hotel.count }
+      end
+
+      context 'some fields are changed' do
+        let(:new_description) { 'new text' }
+        let(:attributes) { { identifier: identifier, description: new_description } }
+
+        it 'updates the existing hotel' do
+          expect{ Hotel.create_from(attributes) }.to change{ Hotel.last.description }.from(nil).to(new_description)
+        end
+      end
+    end
+  end
 end
