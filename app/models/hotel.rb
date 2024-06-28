6cfcfdb168359cc1d41c2968163ebd59b35c246d
Tianhan <tianhand@gmail.com>
2024-06-28 16:25:03 +0800

    add Hotel.data_cleaning

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,8 +1,41 @@
 class Hotel < ApplicationRecord
   validates_presence_of :identifier
 
+  # special handling for alias (patagonia format)
+  alias_attribute :lat, :latitude
+  alias_attribute :lng, :longitude
+  alias_attribute :info, :description
+
+  def self.data_cleaning(input)
+    output = {}
+
+    input.each do |key, value|
+      key = key.to_s.strip.underscore
+
+      # remove prefix (paperflies format)
+      key = key[6..-1] if key.starts_with?('hotel_')
+
+      # skip belongs_to and has_many associations
+      next if key.ends_with?('_id') || key.ends_with?('s') || key == 'destination'
+
+      # special handling for nested attributes (paperflies format)
+      if value.is_a?(Hash)
+        output.merge!(data_cleaning(value))
+        next
+      end
+
+      output[key] = value.is_a?(String) ? value.strip : value
+    end
+
+    # handle identifier and do not touch primary key
+    output['identifier'] ||= output.delete('id')
+
+    return output
+  end
+
   def self.create_from(attributes)
-    hotel = self.where(identifier: attributes[:identifier]).first_or_initialize
+    attributes = data_cleaning(attributes)
+    hotel = Hotel.where(identifier: attributes['identifier']).first_or_initialize
     hotel.update!(attributes)
     return hotel
   end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'input_strings'
 
 RSpec.describe Hotel, type: :model do
   let(:identifier) { 'Abc123' }
@@ -35,7 +36,7 @@ RSpec.describe Hotel, type: :model do
     context 'hotel already exists' do
       let!(:existing_hotel) { Hotel.create(identifier: identifier) }
 
-      it 'does not create new hotel' do
+      it 'does not create a duplicate' do
         expect{ Hotel.create_from(attributes) }.not_to change{ Hotel.count }
       end
 
@@ -47,6 +48,39 @@ RSpec.describe Hotel, type: :model do
           expect{ Hotel.create_from(attributes) }.to change{ Hotel.last.description }.from(nil).to(new_description)
         end
       end
+    end
+  end
+
+  describe '.data_cleaning' do
+    it 'converts keys and strip spaces' do
+      expect(Hotel.data_cleaning(' SomeKey ' => ' some value ')).to include('some_key' => 'some value')
+    end
+
+    it 'converts nested keys and flattens the structure' do
+      expect(Hotel.data_cleaning(' outerKey ' => { ' Inner Key ' => ' inner value ' })).to include('inner key' => 'inner value')
+    end
+
+    it 'does not change primary key' do
+      expect(Hotel.data_cleaning(id: 123).keys).not_to include('id')
+    end
+
+    it 'keeps identifier (acme format)' do
+      input = acme_hotels.sample
+      expect(Hotel.data_cleaning(input)).to include('identifier' => input['Id'])
+    end
+
+    it 'keeps identifier (patagonia format)' do
+      input = patagonia_hotels.sample
+      expect(Hotel.data_cleaning(input)).to include('identifier' => input['id'])
+    end
+
+    it 'keeps identifier (paperflies format)' do
+      input = paperflies_hotels.sample
+      expect(Hotel.data_cleaning(input)).to include('identifier' => input['hotel_id'])
+    end
+
+    it 'can be persisted' do
+      expect{ mixed_hotels.each { |x| Hotel.create!(Hotel.data_cleaning(x)) } }.not_to raise_error
     end
   end
 end
